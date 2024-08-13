5028e301aff9432c0db0ee0337c6748b38b80406
masa0902dev <masa0902dev@gmail.com>
2024-08-13 10:12:29 +0900

    add button-click-feature when keydowning enter in input-field

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Input } from "@mui/material";
-import { FC } from "react";
+import { doc } from "prettier";
+import { FC, useRef } from "react";
 
 interface InputTaskProps {
   task: {
@@ -10,21 +11,35 @@ interface InputTaskProps {
   handleClick: () => void;
 }
 
-const InputTask: FC<InputTaskProps> = ({ task, handleInputChange, handleClick }) => {
+const InputTask: FC<InputTaskProps> = ({
+  task,
+  handleInputChange,
+  handleClick,
+}) => {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && buttonRef.current) {
+      buttonRef.current.focus();
+      buttonRef.current.click();
+    }
+  };
+
   return (
     <Box className="item-width input-wrapper">
       <Input
         type="text"
         value={task.input}
         onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
         placeholder="Enter Task Name"
         color="success"
       />
       <Button
-        variant="contained"
-        color="success"
+        ref={buttonRef}
         onClick={handleClick}
         disabled={!task.input}
+        variant="contained"
+        color="success"
       >
         Set Task
       </Button>
